8e956a4349c9c7fe0b961341ad3c21124f9167e3
IGOR-B50\ingwar <ingwahr@outlook.com>
2018-01-18 17:32:06 +0500

    Liskov - fix!!

---
+++ LiskovSubstitutionalPrinciple/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Reflection.Metadata;
+
+namespace LiskovSubstitutionalPrinciple
+{
+
+    class Rectangle
+    {
+        public virtual int Width { get;  set; }
+        public virtual int Heigth { get;  set; }
+
+        public Rectangle()
+        {
+            
+        }
+        public Rectangle(int width, int heigth)
+        {
+            Width = width;
+            Heigth = heigth;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)} : {Width}, {nameof(Heigth)} : {Heigth}";
+        }
+    }
+
+    class Square : Rectangle
+    {
+        public override int Width
+        {
+            set { base.Width = base.Heigth = value; }
+        }
+        
+        public override int Heigth
+        {
+            set { base.Heigth = base.Width = value; }
+        }
+    }
+    class Program
+    {
+        public static int Area(Rectangle r) => r.Width * r.Heigth;
+        static void Main(string[] args)
+        {
+            var rc = new Rectangle(100,30);
+            
+            Console.WriteLine($"{rc} has area of {Area(rc)}");
+
+            Rectangle sq = new Square();
+            sq.Width = 4;
+            
+            Console.WriteLine($"{sq} has area of {Area(sq)}");
+        }
+    }
+}
